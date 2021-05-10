06a029f39d2e4413c8bd16c042daacb7e465492b
Kstovchanin1902 <78904695+Kstovchanin1902@users.noreply.github.com>
2021-05-10 19:13:45 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,9 +5,98 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+   private:
+        struct Item {
+            T data; 
+            Item* next;
+        };
+    public:
+        LListQueue ():head(nullptr), tail(nullptr) {}
+        ~LListQueue ();
+        void push (const T&);
+        T pop();
+        void print() const;
+    private:
+        LListQueue::Item* create (const T&);
+        Item *head;
+        Item *tail;
 };
-
+template <typename T>
+LListQueue<T>::~LListQueue ()
+{
+    while (head)
+    {
+        pop ();
+    }
+}
+template <typename T>
+void LListQueue<T>::push (const T& inData)
+{
+    if (head && tail)
+    {
+        Item* temp = head;
+        int prior = inData.prior;
+        if (head -> data.prior >= prior) 
+        {
+            while (temp)
+            {
+                if (temp -> next)
+                {
+                    if (prior > temp -> next -> data.prior)
+                    {
+                        Item *inner = create (inData);
+                        inner -> next = temp -> next;
+                        temp -> next = inner;
+                        break;
+                    }
+                }
+                else
+                {
+                    temp -> next = create (inData);
+                    break;
+                }
+                temp = temp -> next;
+            }
+        }
+        else
+        {
+            head = create (inData);
+            head -> next = temp;
+        }
+    }
+    else
+    {
+        head = create(inData);
+        tail = head;
+    }
+}
+template <typename T>
+T LListQueue<T>::pop ()
+{
+    if (head)
+    {
+        Item *temp = head -> next;
+        T data = head -> data;
+        delete head;
+        head = temp;
+        return data;
+    }
+    else
+    {
+        return (T) 0;
+    }
+}
+template <typename T>
+void LListQueue<T>::print () const
+{
+    Item *temp = head;
+    while (temp)
+    {
+        std::cout << "char = " << temp -> data.ch;
+        std::cout << " prior = " << temp -> data.prior << std::endl;
+        temp = temp -> next;
+    }
+}
 struct SYM {
   char ch;
   int  prior;
