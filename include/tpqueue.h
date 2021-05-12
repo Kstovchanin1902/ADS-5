207ae2943fc5b5454401a909c63f9b4dd3b450a0
Kstovchanin1902 <78904695+Kstovchanin1902@users.noreply.github.com>
2021-05-12 19:52:40 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -7,72 +7,60 @@ template<typename T>
 class TPQueue {
    private:
         struct Item {
-            T data; 
+            T data;
             Item* next;
         };
-    public:
-        TPQueue ():head(nullptr), tail(nullptr) {}
-        ~TPQueue ();
-        void push (const T&);
+   public:
+        TPQueue(): head(nullptr), tail(nullptr) {}
+        ~TPQueue();
+        void push(const T&);
         T pop();
         void print() const;
-    private:
-        TPQueue::Item* create (const T&);
+   private:
+        TPQueue::Item* create(const T&);
         Item *head;
         Item *tail;
 };
 template <typename T>
-TPQueue<T>::~TPQueue ()
-{
-    while (head)
-    {
-        pop ();
+TPQueue<T>::~TPQueue() {
+    while (head) {
+        pop();
     }
 }
 template <typename T>
-void TPQueue<T>::push (const T& inData)
-{
-    if (head && tail)
-    {
+void TPQueue<T>::push(const T& inData) {
+    if (head && tail) {
         Item* temp = head;
         int prior = inData.prior;
-        if (head -> data.prior >= prior) 
-        {
-            while (temp)
-            {
-                if (temp -> next)
-                {
-                    if (prior > temp -> next -> data.prior)
-                    {
-                        Item *inner = create (inData);
+        if (head -> data.prior >= prior) {
+            while (temp) {
+                if (temp -> next){
+                    if (prior > temp -> next -> data.prior) {
+                        Item *inner = create(inData);
                         inner -> next = temp -> next;
                         temp -> next = inner;
                         break;
                     }
                 }
-                else
-                {
-                    temp -> next = create (inData);
+                else {
+                    temp -> next = create(inData);
                     break;
                 }
                 temp = temp -> next;
             }
         }
-        else
-        {
-            head = create (inData);
+        else {
+            head = create(inData);
             head -> next = temp;
         }
     }
-    else
-    {
+    else {
         head = create(inData);
         tail = head;
     }
 }
 template <typename T>
-T TPQueue<T>::pop ()
-{
+T TPQueue<T>::pop() {
         Item *temp = head -> next;
         T data = head -> data;
         delete head;
@@ -80,8 +68,7 @@ T TPQueue<T>::pop ()
         return data;
 }
 template <typename T>
-void TPQueue<T>::print () const
-{
+void TPQueue<T>::print() const {
     Item *temp = head;
     while (temp)
     {
@@ -89,8 +76,7 @@ void TPQueue<T>::print () const
     }
 }
 template <typename T>
-typename TPQueue<T>::Item* TPQueue<T>::create(const T& data)
-{
+typename TPQueue<T>::Item* TPQueue<T>::create(const T& data) {
     Item *item = new Item;
     item -> data = data;
     item -> next = nullptr;
